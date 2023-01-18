b0f2cf0dd906c56e3e88734f2c86a8dd14f6ae14
aubin sinpeze <aubinsimpeze01@gmail.com>
2023-01-18 16:44:51 +0200

    Add todolist.js file with the function add , remove and edith tasks

--- src/index.js
+++ src/index.js
@@ -1,41 +1,36 @@
 // import _ from 'lodash';
 import './style.css';
 
-const list = document.querySelector('.lists-of-tasks');
+import TodoList from './modules/todolist.js';
 
-const tasks = [
-  {
-    description: 'Eat food',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Play game',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Work on my project',
-    completed: false,
-    index: 3,
-  },
-];
-const displayToDo = (tasks) => {
-  tasks.forEach((task) => {
-    const li = `<div id="${task.id}" class="list">
-    <input
-      type="checkbox"
-      id="${task.id}"
-      name="task"
-      value="task"
-      ${task.completed ? 'checked' : ''}
-      class="checkbox"
-    />
-    <input
-      type="text" id="${task.id}" class="text-area" name="task" value="${task.description}" />
-    <button class="delete">Delete</button>
-  </div>`;
-    list.innerHTML += li;
-  });
-};
-displayToDo(tasks);
+const list = document.querySelector('.lists-of-tasks');
+const addTodo = document.querySelector('.form');
+const descript = document.querySelector('#title');
+
+const todoList = new TodoList();
+
+addTodo.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (descript.value.trim()) {
+    todoList.addTask(descript.value);
+    todoList.setStorage();
+    todoList.resetIndex();
+    todoList.displayToDo(list);
+    addTodo.reset();
+  }
+});
+
+document.addEventListener('click', (e) => {
+  if (e.target && e.target.classList.contains('delete')) {
+    const id = parseInt(e.target.parentElement.id, 10);
+    todoList.removeList(id);
+    todoList.resetIndex();
+    todoList.setStorage();
+    todoList.displayToDo(list);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  todoList.getStorage();
+  todoList.displayToDo(list);
+});
